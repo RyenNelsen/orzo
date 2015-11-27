da3db7d8f6c2d3f8b95d4fde3905555219e349d5
Ryen Nelsen <ryennelsen@gmail.com>
2015-11-26 22:38:39 -0700

    Add support for generating random characters

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+var NUMBERS = '0123456789';
+var LOWERCASE_CHARACTERS = 'abcdefghijklmnopqrstuvwxyz';
+var UPPERCASE_CHARACTERS = LOWERCASE_CHARACTERS.toUpperCase();
+var SPECIAL_CHARACTERS = '!@#$%^&*()';
+var HEX_CHARACTERS = 'abcdef' + NUMBERS;
+
 function CustomError(name, message) {
     Error.captureStackTrace(this);
     this.name = name;
@@ -23,4 +29,44 @@ function generateRandomNumber(args) {
     return Math.floor(Math.random() * (args.max - args.min) + args.min);
 }
 
+/**
+ * Generates a random set of 10 characters unless a different 'len' is supplied.
+ * @param  {Object} [args]                Contains the arguments for the random string generator.
+ * @param  {number} [args.len=10]         Length for the random string.
+ * @param  {string} [args.char=a-zA-Z0-9] The character set for the random string.
+ * @return {string}                       Random set of characters.
+ */
+function generateRandomString(args) {
+    args = args || { };
+
+    // TODO: Add ability to parse character sets like regex (eg: a-zA-Z)
+
+    args.len = args.len || 10;
+    args.chars = args.chars || NUMBERS + LOWERCASE_CHARACTERS + UPPERCASE_CHARACTERS;
+
+    if (args.len < 1) {
+        throw new RangeError('\'len\' must be greater than 0');
+    }
+    if (typeof args.chars !== 'string') {
+        throw new TypeError('\'chars\' must be a string');
+    }
+
+    var output = '';
+
+    for (var i = 0; i < args.len; i++) {
+        output += args.chars[generateRandomNumber({min: 0, max: args.chars.length})];
+    }
+
+    return output;
+}
+
+// Constant exports
+exports.NUMBERS = NUMBERS;
+exports.LOWERCASE_CHARACTERS = LOWERCASE_CHARACTERS;
+exports.UPPERCASE_CHARACTERS = UPPERCASE_CHARACTERS;
+exports.SPECIAL_CHARACTERS = SPECIAL_CHARACTERS;
+exports.HEX_CHARACTERS = HEX_CHARACTERS;
+
+// Function exports
 exports.dice = generateRandomNumber;
+exports.chars = generateRandomString;
--- test/index.test.js
+++ test/index.test.js
@@ -6,6 +6,7 @@ describe('The main library', function() {
     it('should import', function() {
         assert(orzo);
     });
+
     describe('using the dice feature', function() {
         it('should return a random number between 1 and 6 in all 20 rolls', function() {
             for (var i = 0; i < 20; i++) {
@@ -14,6 +15,24 @@ describe('The main library', function() {
         });
         it('should return an error if max is less than min', function() {
             expect(function() { orzo.dice({min: 6, max: 5}); }).to.throw(RangeError);
+        });
+    });
+
+    describe('using the chars feature', function() {
+        it('should return a string of 10 random characters by default', function() {
+            expect(orzo.chars()).to.have.length(10);
+        });
+        it('should return a string of 5 random characters when supplied the length 5', function() {
+            expect(orzo.chars({len: 5})).to.have.length(5);
+        });
+        it('should return all the same characters if only 1 char is supplied', function() {
+            expect(orzo.chars({chars: 'a'})).to.equal('aaaaaaaaaa');
+        });
+        it('should return an error if length is less than 1', function() {
+            expect(function() { orzo.chars({len: -1}); }).to.throw(RangeError);
+        });
+        it('should return an error if the supplied character set is anything but a string', function() {
+            expect(function() { orzo.chars({chars: 1}) }).to.throw(TypeError);
         });
     });
 });
