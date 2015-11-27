eb5a59522a38aa483223f63d5231a3f44b3091f4
Ryen Nelsen <ryennelsen@gmail.com>
2015-11-26 20:36:00 -0700

    Add documentaion for generateRandomNumber()

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ function CustomError(name, message) {
 
 CustomError.prototype = Object.create(Error.prototype);
 
+/**
+ * Generates a random number between 1 and 6 unless different min/max values are supplied.
+ * @param  {Object} [args]       Contains the arguments for the random number generateRandomNumber
+ * @param  {number} [args.min=1] Lowest possible random number (inclusive).
+ * @param  {number} [args.max=6] Highest possible random number (inclusive).
+ * @return {number}              Random number between min and max.
+ */
 function generateRandomNumber(args) {
     args = args || { min: 1, max: 6 };
 
